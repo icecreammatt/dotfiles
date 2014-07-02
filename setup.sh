98de4373f31f8b8b993be779b42e1fb6bd1fdad1
Matt Carrier <icecreammatt@gmail.com>
2014-07-02 13:03:27 -0700

    Moved oh-my-zsh install to after local changes

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Install oh-my-zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-
 # Add local zshrc
 touch ~/.zshrc_local
 
 # Add local bin if it doesn't exist
 mkdir ~/bin 2> /dev/null
+
+# Install oh-my-zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+echo "Oh-My-Zsh Installed"
 
 # Setup dotfile
 . $HOME/dotfiles/setup_links.sh
