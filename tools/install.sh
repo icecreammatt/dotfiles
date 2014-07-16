e6ac0075f41655ddfdcd3d203c83ebc45df6491e
Matt Carrier <icecreammatt@gmail.com>
2014-07-16 10:46:14 -0700

    Updated install script to include scripts repo

--- tools/install.sh
+++ tools/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export github_username="icecreammatt"
+
 cd $HOME
-git clone https://github.com/icecreammatt/dotfiles
+git clone https://github.com/$github_username/dotfiles
+git clone https://github.com/$github_username/scripts
 . $HOME/dotfiles/setup.sh
