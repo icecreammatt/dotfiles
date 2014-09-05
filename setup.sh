db3aac4cf6ccc909db4a031cf2179c14f112ced2
Matt Carrier <icecreammatt@gmail.com>
2014-09-04 22:05:32 -0700

    Moved vim install to after homebrew install to ensure latest vim before installing plugins requiring vim7.4

--- setup.sh
+++ setup.sh
@@ -12,7 +12,6 @@ echo "Oh-My-Zsh Installed"
 
 # Setup dotfile
 . $HOME/dotfiles/setup_links.sh
-. $HOME/dotfiles/setup_vim.sh
 . $HOME/dotfiles/setup_scripts.sh
 
 case "$OSTYPE" in
@@ -24,6 +23,9 @@ case "$OSTYPE" in
   bsd*)     echo "BSD" ;;
   *)        echo "unknown: $OSTYPE" ;;
 esac
+
+# Install vim after OSX setup to ensure that latest Vim 7.4 is installed.
+. $HOME/dotfiles/setup_vim.sh
 
 # Switch to use ssh instead of http 
 git remote set-url origin git@github.com:$github_username/dotfiles.git
