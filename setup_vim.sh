47d8d15a68f1a9bb6b5c12123e33088103357448
Matt Carrier <icecreammatt@gmail.com>
2015-04-09 10:55:26 -0700

    Moved .swap dir creation up to prevent error which can occur when loading vim plugin installer

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
 mkdir ~/.vim/bundle -p
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Create directory for swapfiles to live
+mkdir $HOME/.vim/swapfiles
 
 vim +PluginInstall +qall
 
@@ -12,5 +14,3 @@ then
     cd ~
 fi
 
-# Create directory for swapfiles to live
-mkdir $HOME/.vim/swapfiles
