a47de776054c236603018fa77a544eb595707f09
Matt Carrier <icecreammatt@gmail.com>
2015-04-09 17:16:42 -0700

    Hide output when checking for dotfile updates

--- tools/check_for_upgrade.sh
+++ tools/check_for_upgrade.sh
@@ -38,7 +38,7 @@ then
     else
 
       pushd $dotfiles 
-      git fetch --all 2> /dev/null
+      git fetch --all &> /dev/null
       originMaster=$(git rev-parse origin/master);
       localHead=$(git rev-parse head);
       popd
