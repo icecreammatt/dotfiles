d64a4f5138bb667e883c8dcd11b74ace4be85a78
Matt Carrier <icecreammatt@gmail.com>
2015-10-31 03:09:50 -0700

    Setup vim after updating git origin

--- setup.sh
+++ setup.sh
@@ -30,11 +30,11 @@ case "$OSTYPE" in
   *)        echo "unknown: $OSTYPE" ;;
 esac
 
-# Install vim after OSX setup to ensure that latest Vim 7.4 is installed.
-. $HOME/dotfiles/setup_vim.sh
-
 # Switch to use ssh instead of http 
 # This is to switch so git push works over ssh instead of https 
 # The reason for this is I have two factor enabled and I cannot push
 # with two factor over HTTPS
 git remote set-url origin git@github.com:$github_username/dotfiles.git
+
+# Install vim after OSX setup to ensure that latest Vim 7.4 is installed.
+. $HOME/dotfiles/setup_vim.sh
